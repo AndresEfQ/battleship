b038ef1a7a01e6334c6df8b50bce609970f28ddc
AndresEfQ <andresf2641@gmail.com>
2022-11-27 19:28:04 -0500

    Implement dom module

--- modules/dom.js
+++ modules/dom.js
@@ -1,3 +1,29 @@
-export default function dom() {
-  this.placeShips
+export default function Dom() {
+
+  let timer;
+  let touchDuration = 1000;
+
+  this.placeShips = function () {
+
+  }
+
+  this.grabShip = function () {
+    console.log('holding ship')
+  }
+
+  this.dragShip = function () {
+
+  }
+
+  this.dropShip = function () {
+
+  }
+
+  this.holdShip = function () {
+    timer = setTimeout(this.grabShip, touchDuration)
+  }
+
+  this.cancelHoldShip = function () {
+    if (timer) clearTimeout(timer);
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Ship from "../modules/ship";
 import Gameboard from "../modules/gameboard";
 import Player from "../modules/player";
+import Dom from "../modules/dom";
 import "./styles/index.css"
 
 const grids = document.getElementsByClassName('grid');
@@ -10,5 +11,13 @@ for (let grid of grids) {
     cell.className = 'cell';
     grid.appendChild(cell);
   }
+}
 
+const shipImages = document.getElementsByClassName('ship-img');
+for (let img of shipImages) {
+
+  let dom = new Dom();
+  img.addEventListener('touchstart', dom.holdShip);
+
+  img.addEventListener('touchcancel', dom.cancelHoldShip);
 }
