c54634b1e6815a250f7aaa115d5274ddcb5bb1c4
AndresEfQ <andresf2641@gmail.com>
2022-11-28 19:43:37 -0500

    Implement grabShip

--- modules/dom.js
+++ modules/dom.js
@@ -1,14 +1,22 @@
-export default function Dom() {
+export default function Dom(gameboard) {
 
-  let timer;
   let touchDuration = 1000;
+  let timer;
 
   this.placeShips = function () {
 
   }
 
-  this.grabShip = function () {
-    console.log('holding ship')
+  this.grabShip = function (ship, gameboard, e) {
+    /* alert('holding ship'); */
+    ship.style.position = 'absolute';
+    console.log(e);
+    ship.style.top = `${e.targetTouches[0].clientY - 390}px`;
+    ship.style.left = `${e.targetTouches[0].clientX - 50}px`;
+    ship.addEventListener('touchmove', (e) => {
+      ship.style.top = `${e.changedTouches[0].clientY - 390}px`;
+      ship.style.left = `${e.changedTouches[0].clientX - 50}px`;
+    });
   }
 
   this.dragShip = function () {
@@ -19,11 +27,21 @@ export default function Dom() {
 
   }
 
-  this.holdShip = function () {
-    timer = setTimeout(this.grabShip, touchDuration)
+  this.holdShip = function (e) {
+    e.preventDefault();
+    let ship = e.target;
+    timer = setTimeout(() => this.grabShip(ship, gameboard, e), touchDuration)
   }
 
-  this.cancelHoldShip = function () {
+  this.cancelHoldShip = function (e) {
+    e.preventDefault();
     if (timer) clearTimeout(timer);
   }
 }
+
+/* Event: x = 291
+          y = 628
+
+   Position: top = 240;
+             left = 240;
+*/
--- src/index.js
+++ src/index.js
@@ -15,9 +15,22 @@ for (let grid of grids) {
 
 const shipImages = document.getElementsByClassName('ship-img');
 for (let img of shipImages) {
-
+  console.log(img);
   let dom = new Dom();
-  img.addEventListener('touchstart', dom.holdShip);
-
-  img.addEventListener('touchcancel', dom.cancelHoldShip);
+  img.addEventListener('touchstart', (e) => dom.holdShip(e));
+  img.addEventListener('touchend', (e) => dom.cancelHoldShip(e));
+  img.addEventListener('mouseup', (e) => dom.cancelHoldShip(e));
+  img.addEventListener('mousedown', (e) => dom.holdShip(e));
 }
+
+const button = document.getElementById('pass');
+button.addEventListener('click', () => alert('pass'));
+
+const testImg = document.getElementById('testImg');
+const testGameboard = document.getElementById('testGameboard');
+testGameboard.addEventListener('click', (e) => {
+  console.log(e.clientX, e.clientY);
+  testImg.style.position = 'absolute';
+  testImg.style.top = e.clientY;
+  testImg.style.top = e.clientX;
+})
